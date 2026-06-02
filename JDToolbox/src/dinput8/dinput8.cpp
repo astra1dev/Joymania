22aa3e95a374a06853520927d35c3ef4686dd6c3
Astral✨ <90265231+astra1dev@users.noreply.github.com>
2026-06-02 20:24:48 +0200

    refactor: split entrypoint.cpp into dllmain.cpp and dinput8.cpp

--- JDToolbox/src/dinput8/dinput8.cpp
+++ JDToolbox/src/dinput8/dinput8.cpp
@@ -1,44 +1,11 @@
 #include <windows.h>
-#include "gui.cpp"
-
-HANDLE hCurrentUIThread = nullptr;
+#include <iostream>
+#include "../logger.h"
+#include "../utils.h"
 
 static HMODULE realDll = nullptr;
 typedef HRESULT (WINAPI *PFN_DirectInput8Create)(HINSTANCE hinst, DWORD dwVersion, REFIID riid, LPVOID* ppvOut, LPUNKNOWN punkOuter);
 static PFN_DirectInput8Create real_DirectInput8Create = nullptr;
-
-/// DLL entry point, see https://learn.microsoft.com/en-us/windows/win32/dlls/dllmain
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-    switch (fdwReason)
-    {
-        case DLL_PROCESS_ATTACH:
-            //MessageBoxA(NULL, "[JDToolbox] DLL Loaded Successfully!", "Notification", MB_OK | MB_ICONINFORMATION);
-            hCurrentUIThread = CreateThread(NULL, 0, ImGuiThread, NULL, 0, NULL);
-            Logger::Log("DllMain called with DLL_PROCESS_ATTACH");
-            break;
-        case DLL_THREAD_ATTACH:
-            // Thread-specific initialization code here
-            break;
-        case DLL_THREAD_DETACH:
-            // Thread-specific cleanup code here
-            break;
-        case DLL_PROCESS_DETACH:
-            Logger::Log("DllMain called with DLL_PROCESS_DETACH (lpvReserved=" + FormatHex((uintptr_t)lpvReserved) + ")");
-
-            TerminateThread(hCurrentUIThread, 0);
-
-            if (realDll != nullptr)
-            {
-                real_DirectInput8Create = nullptr;
-
-                FreeLibrary(realDll);
-                realDll = nullptr;
-            }
-
-            break;
-    }
-    return TRUE; // Successful initialization
-}
 
 extern "C" HRESULT __stdcall DirectInput8Create(HINSTANCE hinst, DWORD dwVersion, REFIID riid, LPVOID* ppvOut, LPUNKNOWN punkOuter)
 {
---
+++ JDToolbox/src/dllmain.cpp
@@ -0,0 +1,38 @@
+#include <windows.h>
+#include "gui.cpp"
+#include "dinput8/dinput8.cpp"
+
+HANDLE hCurrentUIThread = nullptr;
+
+/// DLL entry point, see https://learn.microsoft.com/en-us/windows/win32/dlls/dllmain
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    switch (fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            //MessageBoxA(NULL, "[JDToolbox] DLL Loaded Successfully!", "Notification", MB_OK | MB_ICONINFORMATION);
+            hCurrentUIThread = CreateThread(NULL, 0, ImGuiThread, NULL, 0, NULL);
+            Logger::Log("DllMain called with DLL_PROCESS_ATTACH");
+            break;
+        case DLL_THREAD_ATTACH:
+            // Thread-specific initialization code here
+            break;
+        case DLL_THREAD_DETACH:
+            // Thread-specific cleanup code here
+            break;
+        case DLL_PROCESS_DETACH:
+            Logger::Log("DllMain called with DLL_PROCESS_DETACH (lpvReserved=" + FormatHex((uintptr_t)lpvReserved) + ")");
+
+            TerminateThread(hCurrentUIThread, 0);
+
+            if (realDll != nullptr)
+            {
+                real_DirectInput8Create = nullptr;
+
+                FreeLibrary(realDll);
+                realDll = nullptr;
+            }
+
+            break;
+    }
+    return TRUE; // Successful initialization
+}
